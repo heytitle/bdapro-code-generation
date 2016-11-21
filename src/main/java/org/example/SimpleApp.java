7674715c2e1d46648fe6de4a40a6179d7f602d9a
heytitle <pat.chormai@gmail.com>
2016-11-22 00:27:08 +0100

    Init

---
+++ src/main/java/org/example/SimpleApp.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.common.typeutils.base.IntSerializer;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.runtime.TupleSerializer;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+
+import org.apache.flink.runtime.operators.sort.FixedLengthRecordSorter;
+import org.apache.flink.runtime.operators.sort.InMemorySorter;
+
+import java.util.ArrayList;
+
+public class SimpleApp {
+	private static TypeSerializer<Tuple2<Integer,Integer>> serializer;
+
+	public static void main(String[] args) {
+
+		System.out.println("HI!");
+
+		ArrayList<Tuple2<Integer,Integer>> dummyData = generateDummyData(10);
+		System.out.println(dummyData);
+
+		int[] fields = new int[]{4, 4, 3};
+
+
+		TypeInformation<?>[] fieldTypes = new TypeInformation[2];
+		fieldTypes[0] = TypeInformation.of(Integer.class);
+		fieldTypes[1] = TypeInformation.of(Integer.class);
+
+		serializer = new TupleSerializer<Tuple2<Integer, Integer>>(
+				(Class<Tuple2<Integer, Integer>>) (Class<?>) Tuple2.class,
+				new TypeSerializer[] {
+						IntSerializer.INSTANCE,
+						IntSerializer.INSTANCE
+				}
+		);
+
+
+
+	}
+
+	private	static ArrayList<Tuple2<Integer,Integer>> generateDummyData(int size ){
+		ArrayList<Tuple2<Integer,Integer>> data = new ArrayList<Tuple2<Integer,Integer>>();
+
+		while(size > 0 ){
+			Tuple2 t = new Tuple2(1,2);
+			data.add(t);
+			size--;
+		}
+
+		return data;
+	}
+
+	private InMemorySorter createSorter(){
+
+//		FixedLengthRecordSorter<IntPair> b = new FixedLengthRecordSorter<IntPair>()
+
+	}
+}
