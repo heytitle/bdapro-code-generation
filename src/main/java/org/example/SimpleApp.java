06ea51ac02dbea2c9a9a4fbdcbd758fadfbbf36f
heytitle <pat.chormai@gmail.com>
2016-11-25 11:09:09 +0100

    experiment FixedLegthRecordSorter w/ tuple

--- src/main/java/org/example/SimpleApp.java
+++ src/main/java/org/example/SimpleApp.java
@@ -1,18 +1,25 @@
 package org.example;
 
+import org.apache.flink.api.common.typeutils.TypeComparator;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.common.typeutils.base.IntComparator;
 import org.apache.flink.api.common.typeutils.base.IntSerializer;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.runtime.TupleComparator;
 import org.apache.flink.api.java.typeutils.runtime.TupleSerializer;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 
+import org.apache.flink.core.memory.MemorySegment;
+import org.apache.flink.core.memory.MemorySegmentFactory;
 import org.apache.flink.runtime.operators.sort.FixedLengthRecordSorter;
 import org.apache.flink.runtime.operators.sort.InMemorySorter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleApp {
-	private static TypeSerializer<Tuple2<Integer,Integer>> serializer;
+	private static final int pageSize = 32*1024;
+	private static final int numSegments = 34;
+
 
 	public static void main(String[] args) {
 
@@ -21,17 +28,11 @@ public class SimpleApp {
 		ArrayList<Tuple2<Integer,Integer>> dummyData = generateDummyData(10);
 		System.out.println(dummyData);
 
-		TypeInformation<?>[] fieldTypes = new TypeInformation[2];
-		fieldTypes[0] = TypeInformation.of(Integer.class);
-		fieldTypes[1] = TypeInformation.of(Integer.class);
 
-		serializer = new TupleSerializer<Tuple2<Integer, Integer>>(
-				(Class<Tuple2<Integer, Integer>>) (Class<?>) Tuple2.class,
-				new TypeSerializer[] {
-						IntSerializer.INSTANCE,
-						IntSerializer.INSTANCE
-				}
-		);
+		System.out.print("Creating sorter");
+
+		InMemorySorter sorter = createSorter();
+
 
 
 
@@ -49,9 +50,46 @@ public class SimpleApp {
 		return data;
 	}
 
-	private InMemorySorter createSorter(){
+	public static InMemorySorter createSorter() {
+		TypeSerializer[] typeSerializer = new TypeSerializer[] {
+				IntSerializer.INSTANCE,
+				IntSerializer.INSTANCE
+		};
 
-//		FixedLengthRecordSorter<IntPair> b = new FixedLengthRecordSorter<IntPair>()
+		TypeSerializer<Tuple2<Integer,Integer>> tupleSerializer = new TupleSerializer<Tuple2<Integer, Integer>>(
+				(Class<Tuple2<Integer, Integer>>) (Class<?>) Tuple2.class,
+				typeSerializer
+		);
 
+
+		IntComparator intComparator = new IntComparator(true);
+
+		TupleComparator<Tuple2<Integer,Integer>> comparator = new TupleComparator<Tuple2<Integer, Integer>>(
+				new int[]{0},
+				new TypeComparator[]{
+					intComparator
+				},
+				typeSerializer
+		);
+
+		MemorySegment memory = MemorySegmentFactory.allocateUnpooledSegment(pageSize);
+
+		FixedLengthRecordSorter<Tuple2<Integer,Integer>> sorter = new FixedLengthRecordSorter<Tuple2<Integer, Integer>>(
+			tupleSerializer,
+			comparator,
+			getMemory( numSegments, pageSize )
+		);
+
+		return  sorter;
 	}
+
+
+	private static List<MemorySegment> getMemory(int numSegments, int segmentSize) {
+		ArrayList<MemorySegment> list = new ArrayList<MemorySegment>(numSegments);
+		for (int i = 0; i < numSegments; i++) {
+			list.add(MemorySegmentFactory.allocateUnpooledSegment(segmentSize));
+		}
+		return list;
+	}
+
 }
