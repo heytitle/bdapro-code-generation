8cc17c0e08e4513f76e377cb98209c02c7242cb8
heytitle <pat.chormai@gmail.com>
2016-11-28 14:35:38 +0100

    tuple2 and normalizedkeysorter

--- src/main/java/org/example/SimpleApp.java
+++ src/main/java/org/example/SimpleApp.java
@@ -10,22 +10,18 @@ import org.apache.flink.api.java.typeutils.runtime.TupleSerializer;
 
 import org.apache.flink.core.memory.MemorySegment;
 import org.apache.flink.core.memory.MemorySegmentFactory;
-import org.apache.flink.runtime.operators.sort.FixedLengthRecordSorter;
 import org.apache.flink.runtime.operators.sort.InMemorySorter;
+import org.apache.flink.runtime.operators.sort.NormalizedKeySorter;
 import org.apache.flink.runtime.operators.sort.QuickSort;
 import org.apache.flink.util.MutableObjectIterator;
-import org.example.utils.IntPair;
-import org.example.utils.IntPairComparator;
-import org.example.utils.IntPairSerializer;
-import org.example.utils.RandomIntPairGenerator;
-import scala.Int;
+import org.example.utils.generator.RandomIntTuple2;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleApp {
-	private static final int pageSize = 32*1024;
+	private static final int pageSize    = 32*1024;
 	private static final int numSegments = 34;
 	private static final int NUM_RECORDS = 10;
 	private static final int SEED	     = 11;
@@ -33,14 +29,15 @@ public class SimpleApp {
 
 	public static void main(String[] args) throws Exception {
 
-		RandomIntPairGenerator generator = new RandomIntPairGenerator(SEED);
+		RandomIntTuple2 generator = new RandomIntTuple2(SEED);
 
 		InMemorySorter sorter = createSorter();
 
 		int num = -1;
-		IntPair record = new IntPair();
+		Tuple2<Integer,Integer> record = new Tuple2<Integer,Integer>();
 		do {
 			generator.next(record);
+			System.out.println(record);
 			num++;
 		}
 		while (sorter.write(record) && num < NUM_RECORDS);
@@ -53,12 +50,27 @@ public class SimpleApp {
 
 	public static InMemorySorter createSorter() {
 
+		TupleSerializer<Tuple2<Integer,Integer>> serializer = new TupleSerializer<Tuple2<Integer, Integer>>(
+				(Class<Tuple2<Integer, Integer>>) (Class<?>) Tuple2.class,
+				new TypeSerializer[] {
+						IntSerializer.INSTANCE,
+						IntSerializer.INSTANCE
+				}
+		);
 
-		IntPairSerializer serializer = new IntPairSerializer();
-		IntPairComparator comparator = new IntPairComparator();
+		TupleComparator<Tuple2<Integer,Integer>> comparator = new TupleComparator<Tuple2<Integer, Integer>>(
+				new int[]{0},
+				new TypeComparator[]{
+					new IntComparator(true)
+				},
+				new TypeSerializer[] {
+					IntSerializer.INSTANCE
+				}
+		);
+
 		MemorySegment memory = MemorySegmentFactory.allocateUnpooledSegment(pageSize);
 
-		FixedLengthRecordSorter<IntPair> sorter = new FixedLengthRecordSorter<IntPair>(
+		NormalizedKeySorter<Tuple2<Integer,Integer>> sorter = new NormalizedKeySorter<Tuple2<Integer,Integer>>(
 			serializer,
 			comparator,
 			getMemory( numSegments, pageSize )
@@ -76,17 +88,17 @@ public class SimpleApp {
 		return list;
 	}
 
-	private static void checkCorrectness(MutableObjectIterator<IntPair> iter) throws Exception{
-		IntPair readTarget = new IntPair();
+	private static void checkCorrectness(MutableObjectIterator<Tuple2<Integer,Integer>> iter) throws Exception{
+		Tuple2<Integer,Integer> readTarget = new Tuple2<Integer,Integer>();
 
 		int current;
 		int last;
 
 		iter.next(readTarget);
-		last = readTarget.getKey();
+		last = readTarget.getField(0);
 
 		while ((readTarget = iter.next(readTarget)) != null) {
-			current = readTarget.getKey();
+			current = readTarget.getField(0);
 
 			final int cmp = last - current;
 			if (cmp > 0) {
