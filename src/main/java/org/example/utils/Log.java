59bfefb7d30819c90ee28bce607161a890687bdd
heytitle <pat.chormai@gmail.com>
2016-12-04 16:33:35 +0100

    Add MySort

---
+++ src/main/java/org/example/utils/Log.java
@@ -0,0 +1,19 @@
+package org.example.utils;
+
+/**
+ * Created by heytitle on 12/4/16.
+ */
+public class Log {
+
+	private static boolean is_debugging = Boolean.parseBoolean(System.getenv("JAVA_DEBUG"));
+
+	public static void debug(String msg){
+		if(is_debugging){
+			StackTraceElement st = new Exception().getStackTrace()[1];
+			String caller = st.getClassName();
+			String mn = st.getMethodName();
+			String str = String.format("[%s:%s] %s", caller, mn, msg);
+			System.out.println(str);
+		}
+	}
+}
