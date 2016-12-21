39e576d4fb76d847c2cf9a8c352ec7e7e83f6e57
heytitle <pat.chormai@gmail.com>
2016-12-21 22:08:53 +0100

    introduce test

---
+++ src/test/java/SortTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by heytitle on 12/21/16.
+ */
+
+import junit.framework.TestCase;
+
+public class SortTest extends TestCase {
+	public void testSimple(){
+		assertEquals("Simple Test", 7,7);
+	}
+}
