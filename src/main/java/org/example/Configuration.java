cc88023aae2eb640358aa49c56c0a2549d65dca8
heytitle <pat.chormai@gmail.com>
2016-12-21 22:56:35 +0100

    update conflg

--- src/main/java/org/example/Configuration.java
+++ src/main/java/org/example/Configuration.java
@@ -4,12 +4,11 @@ package org.example;
  * Created by heytitle on 11/28/16.
  */
 public class Configuration {
-	public static final int NUM_RECORDS = 1000;
-	public static final int pageSize    = 32*1024;
-	public static final int numSegments = 34;
+	public static final long NUM_RECORDS = 10000;
+	public static final int pageSize     = 32*1024; // bytes
+	public static final int numSegments  = 1000;
 	public static final int SEED	     = 11;
 
-	public static final int BENCHMARK_RECORD_SIZE = 50000;
-	public static final int BENCHMARK_WARMUP= 3;
-	public static final int BENCHMARK_ITERATION = 10;
+	public static final int BENCHMARK_WARMUP = 20;
+	public static final int BENCHMARK_ITERATION = 100;
 }
